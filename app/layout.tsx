192f561759e93cf6d234221d6b5ad3e062c3fa36
Rafael Siqueira <rafaelsiqueira.dev@gmail.com>
2025-08-29 21:07:44 -0300

    feat: add umami

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Roboto } from "next/font/google"
 import { Suspense } from "react"
 import { ThemeProvider } from "@/components/theme-provider"
 import "./globals.css"
+import Head from "next/head"
 
 export const metadata: Metadata = {
   title: "Social - Rede Social Brasileira",
@@ -23,6 +24,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR" suppressHydrationWarning>
+      <Head>
+        <script
+          defer
+          src="http://social-media-umami-b5999a-145-223-31-54.traefik.me/script.js"
+          data-website-id="54e4289c-95df-4f6d-8094-29b6b9aac498"
+        ></script>
+      </Head>
       <body className={roboto.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <Suspense fallback={null}>{children}</Suspense>
